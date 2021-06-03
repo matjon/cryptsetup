886ee6da0fcb56dc6f92bf5d1120ef78b4b96dcb
Mateusz Jończyk <mat.jonczyk@o2.pl>
2021-06-03 16:29:56 +0200

    Rozdzielenie diskcryptor_phdr i dyskcryptor_enchdr

--- lib/diskcryptor/diskcryptor.h
+++ lib/diskcryptor/diskcryptor.h
@@ -25,13 +25,19 @@
 #define DISKCRYPTOR_HDR_WHOLE_LEN    2048
 #define DISKCRYPTOR_HDR_ENC_LEN    (DISKCRYPTOR_HDR_WHOLE_LEN - DISKCRYPTOR_HDR_SALT_LEN)
 
-struct diskcryptor_phdr {
+struct diskcryptor_enchdr {
 	char salt[DISKCRYPTOR_HDR_SALT_LEN];
-	char encrypted[DISKCRYPTOR_HDR_LEN];
+	char encrypted[DISKCRYPTOR_HDR_ENC_LEN];
 } __attribute__((__packed__));
 
-
 #define DISKCRYPTOR_HDR_KEY_LEN 64
+
+struct diskcryptor_phdr {
+	char _trash[DISKCRYPTOR_HDR_SALT_LEN];
+	char signature[4];
+        char padding[DISKCRYPTOR_HDR_WHOLE_LEN - DISKCRYPTOR_HDR_SALT_LEN - 4];
+} __attribute__((__packed__));
+
 
 struct crypt_device;
 struct crypt_params_diskcryptor;
@@ -39,7 +45,5 @@ struct crypt_params_diskcryptor;
 int DISKCRYPTOR_read_phdr(struct crypt_device *cd,
 		     struct diskcryptor_phdr *hdr,
 		     struct crypt_params_diskcryptor *params);
-
-
 
 #endif
